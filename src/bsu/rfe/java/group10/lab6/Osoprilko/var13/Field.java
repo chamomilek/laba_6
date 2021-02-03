441ea390cfd16d5124eadf19ab5438c3110aa4e5
Karina Osoprilko <chamomileekk@gmail.com>
2021-02-03 22:13:16 +0300

    1class

--- src/bsu/rfe/java/group10/lab6/Osoprilko/var13/Field.java
+++ src/bsu/rfe/java/group10/lab6/Osoprilko/var13/Field.java
@@ -27,6 +27,42 @@ public class Field extends JPanel {
 // Запустить таймер перерисовки области
         repaintTimer.start();
     }
-
+    public void addBall() {
+// Заключается в добавлении в список нового экземпляра BouncingBall
+// Всю инициализацию BouncingBall выполняет сам в конструкторе
+        balls.add(new BouncingBall(this));
+    }
+    // Унаследованный от JPanel метод перерисовки компонента
+    public void paintComponent(Graphics g) {
+// Вызвать версию метода, унаследованную от предка
+        super.paintComponent(g);
+        Graphics2D canvas = (Graphics2D) g;
+// Последовательно запросить прорисовку от всех мячей, хранимых в списке
+        for (BouncingBall ball: balls) {
+            ball.paint(canvas);
+        }
+    }
+    // Синхронизированный, т.е. только 1 поток может одновременно быть внутри
+    public synchronized void pause() {
+// Включить режим паузы
+        paused = true;
+    }
+    // Синхронизированный метод проверки, может ли мяч двигаться
+// (не включен ли режим паузы?)
+    public synchronized void canMove(BouncingBall ball)
+            throws InterruptedException {
+        if (paused) {
+// Если режим паузы включен, то поток, зашедший внутрь
+// данного метода, засыпает
+            wait();
+        }
+    }
+    // Синхронизированный, т.е. только 1 поток может одновременно быть внутри
+    public synchronized void resume() {
+// Выключить режим паузы
+        paused = false;
+// Будим все ожидающие продолжения потоки
+        notifyAll();
+    }
 
 }
